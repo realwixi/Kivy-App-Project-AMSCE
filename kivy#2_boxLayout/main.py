23a85a9c1a14fc7da3f00294e69c1acd791af669
realwixi <98758831+realwixi@users.noreply.github.com>
2023-09-13 03:02:20 +0530

    Add files via upload
    2nd lecture of first 40min from freecodecamp

---
+++ kivy#2_boxLayout/main.py
@@ -0,0 +1,19 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+
+
+class BoxLayoutExample(BoxLayout):
+    pass
+
+
+class MainWidget(Widget):
+    pass
+
+
+class TheLabApp(App):
+    pass
+
+
+TheLabApp().run()
