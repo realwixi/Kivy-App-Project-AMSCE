7e839e21db315d50374b5bb12ab2240e34f8e43d
realwixi <98758831+realwixi@users.noreply.github.com>
2023-09-13 10:26:24 +0530

    Add files via upload
    all about grid layout

---
+++ kivy#4_GridLayout/main.py
@@ -0,0 +1,21 @@
+from kivy.app import App
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+
+
+class BoxLayoutExample(BoxLayout):
+    pass
+class AnchorLayoutExample(AnchorLayout):
+    pass
+
+class MainWidget(Widget):
+    pass
+
+
+class TheLabApp(App):
+    pass
+
+
+TheLabApp().run()
